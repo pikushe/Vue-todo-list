15a71da602e7cff7e24ad12bbde3d97ef91f5e05
Pikushe <piikushe@gmail.com>
2022-05-12 01:15:45 +0300

    feat(responsive): Uygulama için responsive eklendi

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
     <div class="w-1/2 mx-auto">
-      <div class="flex justify-between">
-        <div class="text-2xl"><h1>To-do List</h1></div>
-        <div class="flex flex-row items-center text-xs">
+      <div class="flex justify-between text-xs lg:text-sm">
+        <div class="text-lg lg:text-2xl"><h1>To-do List</h1></div>
+        <div class="flex flex-col sm:flex-row items-center ">
           <input type="checkbox" class="mr-1" v-model="hide">
           <p class="text-blue-700">
             Gizlenenleri göster
           </p>
         </div>
       </div>
-      <div class="flex flex-row justify-center ">
+      <div class="flex flex-row justify-center">
         <div>
-          <input class="bg-blue-300 rounded-l-lg outline-none p-2 w-64" maxlength="24" v-model="newItemTitle" required>
+          <input class="bg-blue-300 rounded-l-lg outline-none p-1 lg:p-2 w-40 sm:w-56 lg:w-64 " maxlength="24" v-model="newItemTitle" required>
         </div>
         <div>
-          <button class="bg-blue-600 rounded-r-lg p-2 text-amber-200" @click="addNewItem">Ekle</button>
+          <button class="bg-blue-600 rounded-r-lg p-1 lg:p-2 text-amber-200" @click="addNewItem">Ekle</button>
         </div>
       </div>
       <hr>
-        <div class="flex flex-row justify-around px-3 py-2" v-for="list in listHidden" :key="list.lists">
+        <div class="flex flex-col sm:flex-row shadow-3xl justify-between px-3 py-2 my-2 text-center" v-for="list in listHidden" :key="list.lists">
           <div>{{list.task}}</div>
-          <div><input type="checkbox" v-model="list.comp" ></div>
-          <button class="bg-red-700 p-1.5 rounded text-amber-300" @click="deleteItem">Sil</button>
+          <div class="m-1"><input type="checkbox" v-model="list.comp" ></div>
+          <button class="bg-red-700 p-1 lg:p-1.5 rounded text-amber-300" @click="deleteItem">Sil</button>
         </div>
     </div>
   </div>
